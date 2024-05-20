912a553b338706e9eab07f8322ff49478468aa41
SeanLee00 <57080931+SeanLee00@users.noreply.github.com>
2024-05-20 20:56:52 +0900

    Finish

--- app/src/main/java/com/example/mvvm_shate_data/CountViewModel.kt
+++ app/src/main/java/com/example/mvvm_shate_data/CountViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.mvvm_shate_data
 
-class CountViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CountViewModel : ViewModel() {
+
+    private val counter = MutableLiveData<Int>()
+
+    init {
+        counter.value = 0
+    }
+
+    fun increment() {
+        counter.value = (counter.value ?: 0) + 1
+    }
+
+    fun decrement() {
+        counter.value = (counter.value ?: 0) - 1
+    }
+
+    fun getCounter(): LiveData<Int> = counter
 }
--- app/src/main/java/com/example/mvvm_shate_data/SecondFragment.kt
+++ app/src/main/java/com/example/mvvm_shate_data/SecondFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.mvvm_shate_data.databinding.FragmentSecondBinding
 
@@ -14,6 +16,7 @@ import com.example.mvvm_shate_data.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private lateinit var viewModel: CountViewModel
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,8 +35,19 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity()).get(CountViewModel::class.java)
+
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+
+
+        viewModel.getCounter().observe(viewLifecycleOwner, Observer {
+            binding.textviewSecond.text = it.toString()
+        })
+
+        binding.decrementButton.setOnClickListener() {
+            viewModel.decrement()
         }
     }
 
